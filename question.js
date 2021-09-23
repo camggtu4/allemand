20ae425bd859d39edbec2a40a39e6d93422109c5
camggtu4 <91284731+camggtu4@users.noreply.github.com>
2021-09-23 21:16:40 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,94 @@
+
+//arrays of Objects
+const quiz = [
+	{
+		q:'Comment dit-on être ?',
+		options:['sein', 'bin','gehen',"sehen" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on le souvenir ?',
+		options:['zeigen', 'Erinnerung','nah',"jede" ],
+		answer:1
+	},
+		{
+		q:'Comment dit-on le modèle ?',
+		options:['zeigen', 'sehr oft','das Vorbild'," das Geschenk" ],
+		answer:2
+	},
+	{
+		q:'Que veut dire zeigen ?',
+		options:['montrer', 'cacher','courrir',"manger" ],
+		answer:0
+	},
+	{
+		q:'Que veut dire Urgossmutter ?',
+		options:['mamie', 'la Belle-mère','grand-mère',"arrière grand-mère" ],
+		answer:3
+	},
+	{
+		q:'Que veut dire deshalb ?',
+		options:["c est pourquoi ", 'à cause de','donc',"ensuite" ],
+		answer:0
+	},
+
+	{
+		q:'Comment dit-on la forêt ?',
+		options:["das buch ", 'der Wald','die Zeitschrift',"Rechner" ],
+		answer:1
+	},
+	{
+		q:'Comment dit-on téléphoner à ?',
+		options:["senden" ,'Telefon abheben','an rufen',"Rechner" ],
+		answer:2
+	},
+
+	{
+		q:'Comment dit-on le beau-père ?',
+		options:["der Grossvater " ,'der vater','der Schwiegervater',"der Enkel" ],
+		answer:2
+	},
+		{
+		q:'Comment dit-on le cousin ?',
+		options:["der cousin " ,'der vater','der Enkel',"der bruder" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on la belle-mère ?',
+		options:["die oma " ,'die grossmutter','die Schwiegermutter',"die Stiefmutter" ],
+		answer:3
+	},
+	{
+		q:'Comment dit-on plus jeune?',
+		options:["jünger " ,' älter','nah',"geschieden" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on divorcé ?',
+		options:["sehr oft " ,' brauchen','Erinnernung',"geschieden" ],
+		answer:3
+	},
+	{
+		q:'Comment dit-on avoir besoin de?',
+		options:["wollen " ,' jede','brauchen',"fragen" ],
+		answer:2
+	},
+	{
+		q:'Comment dit-on enfant unique ?',
+		options:["Einzelkind " ,' Enkel','einziger Sohn',"Mädchen" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on passer du temps ?',
+		options:["verstehen " ,' verbringen','verdammen',"vertrauen" ],
+		answer:1
+	},
+
+
+	{
+		q:'Comment dit-on comprendre ?',
+		options:['deshalb', 'verbringen','fragen',"verstehen" ],
+		answer:3
+	}
+
+]
