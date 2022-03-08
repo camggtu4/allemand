de759d46ff77f9ab49e94276105683df99c1c119
camggtu4 <91284731+camggtu4@users.noreply.github.com>
2022-03-08 22:37:47 +0100

    Add files via upload

---
+++ spc.js
@@ -0,0 +1,4 @@
+const chapitre1 =" Chapitre 1-La vie et l organisation du vivant";
+const chap1I = 'I/Une organisation en lien avec les grandes fonctions biologiques';
+// document.querySelector(".title-chapitre1").innerHTML = chapitre1;
+// document.querySelector(".title-chap1I").innerHTML = chap1I;
