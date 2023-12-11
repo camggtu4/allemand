378eb37022b54feb515a13505cf37863c8d68181
camggtu4 <91284731+camggtu4@users.noreply.github.com>
2023-12-12 00:26:27 +0100

    New world liste of question.js

--- question.js
+++ question.js
@@ -1,30 +1,30 @@
- 
+
 //arrays of Objects
 const quiz = [
 	{
-		q:'Comment dit-on être ?',
-		options:['sein', 'bin','gehen',"sehen" ],
+		q:'Comment dit-on la surveillance ?',
+		options:['Überwachung', 'Ansehen','Absehen',"Übersehen" ],
 		answer:0
 	},
 	{
-		q:'Comment dit-on le souvenir ?',
-		options:['zeigen', 'Erinnerung','nah',"jede" ],
+		q:'Que veut dire die Auslandsreise ?',
+		options:['la langue étrangère', 'Le voyage à l\'étranger','Accueilir un étranger',"Le pays" ],
 		answer:1
 	},
 		{
-		q:'Comment dit-on le modèle ?',
-		options:['zeigen', 'sehr oft','das Vorbild'," das Geschenk" ],
-		answer:2
+		q:'Comment dit-on condamner ?',
+		options:['zeigen', 'verurteilen','verbiden'," erpressen" ],
+		answer:1
 	},
 	{
-		q:'Que veut dire zeigen ?',
-		options:['montrer', 'cacher','courrir',"manger" ],
+		q:'Comment dit-on Le travail principal ?',
+		options:['die Hauptaufgabe', 'die Zeitschrift','Die Hauptarbeit',"Der Stuhl" ],
 		answer:0
 	},
 	{
-		q:'Que veut dire Urgrossmutter ?',
-		options:['mamie', 'la Belle-mère','grand-mère',"arrière grand-mère" ],
-		answer:3
+		q:'Comment dit-on le capitaine ?',
+		options:['Rechner', 'der Hauptmann','der Schriftsteller',"besorgt" ],
+		answer:1
 	},
 	{
 		q:'Que veut dire deshalb ?',
@@ -33,149 +33,190 @@ const quiz = [
 	},
 
 	{
-		q:'Comment dit-on la forêt ?',
-		options:["das buch ", 'der Wald','die Zeitschrift',"Rechner" ],
-		answer:1
+		q:'Comment dit-on la liberté ?',
+		options:["die Freiheit ", 'der Wald','die Zeitschrift',"Die Welt" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on téléphoner à ?',
-		options:["senden" ,'Telefon abheben','an rufen',"Rechner" ],
-		answer:2
+		q:'Comment dit-on menacer ?',
+		options:["bedrohen" ,'senden','verbiden',"erpressen" ],
+		answer:0
 	},
 
 	{
-		q:'Comment dit-on le beau-père ?',
-		options:["der Grossvater " ,'der vater','der Schwiegervater',"der Enkel" ],
-		answer:2
+		q:'Comment dit-on manifester ?',
+		options:["demonstrieren " ,'bedrohen','brauchen',"geschieden" ],
+		answer:0
 	},
 		{
-		q:'Comment dit-on le cousin ?',
-		options:["der cousin " ,'der vater','der Enkel',"der bruder" ],
+		q:'Comment dit-on la violence ?',
+		options:["die Gewalt " ,'der Wald','Die Welt',"die Zeitschrift" ],
 		answer:0
 	},
 	{
-		q:'Comment dit-on la belle-mère ?',
-		options:["die oma " ,'die grossmutter','die Schwiegermutter',"die Stiefmutter" ],
-		answer:3
-	},
-	{
-		q:'Comment dit-on plus jeune?',
-		options:["jünger " ,' älter','nah',"geschieden" ],
+		q:'Comment dit-on arrêter qqn ?',
+		options:["jdn fest " ,'jdm zeigen','jdm  verurteilen',"jdm erpressen" ],
 		answer:0
 	},
 	{
-		q:'Comment dit-on divorcé ?',
-		options:["sehr oft " ,' brauchen','Erinnernung',"geschieden" ],
-		answer:3
-	},
-	{
-		q:'Comment dit-on avoir besoin de?',
-		options:["wollen " ,' jede','brauchen',"fragen" ],
-		answer:2
-	},
-	{
-		q:'Comment dit-on enfant unique ?',
-		options:["Einzelkind " ,' Enkel','einziger Sohn',"Mädchen" ],
+		q:'Comment dit-on se taire ?',
+		options:["schweigen " ,' verbringen','verurteilen',"vertrauen" ],
 		answer:0
 	},
 	{
-		q:'Comment dit-on passer du temps ?',
-		options:["verstehen " ,' verbringen','verdammen',"vertrauen" ],
-		answer:1
-	},
-	{
-		q:'Comment dit-on marié ?',
-		options:["verheiratet " ,' geschieden','verdammen',"vertrauen" ],
+		q:'Comment dit-on le dossier ?',
+		options:["die Akten " ,' das Buch','Das Heft',"Der Stift" ],
 		answer:0
 	},
 	{
-		q:'Comment dit-on offrir du temps ?',
-		options:["Zeit schenken" ,' verheiratet',' Zeit verheiratet',"Zeit  vertrauen" ],
-		answer:1
+		q:'Que veut dire gründen ?',
+		options:["fonder " ,'menacer','se taire',"arrêter qqn" ],
+		answer:0
 	},
+	{
+		q:'Que veut dire enfant das Verhalten  ?',
+		options:["le comportement " ,' menacer','croire',"comprendre" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on remarquer ?',
+		options:["bemerken " ,' verbringen','verdammen',"vertrauen" ],
+		answer:0
+	},
+	{
+		q:'Que veut dire gefährlich ?',
+		options:["dangereux " ,' peureux','menaçant',"intéressant" ],
+		answer:0
+	},
+	{
+		q:'Que veut dire die Bühne ?',
+		options:["la scène" ,' le brouillard','solitaire',"le cauchemar" ],
+		answer:0
+	},
+
 	{ 
-		q:'Traduis je vais me promener',
-		options:["Ich gehe spazieren" ,' Ich spazieren gehe ',' Ich spazieren',"Ich spaziere" ],
-		answer:1
+		q:'Comment dit-on condamner',
+		options:["verurteilen" ,' verbringen ',' verstehen',"fragen" ],
+		answer:0
 	},
 	{
-        q:'Traduire enceinte',
-        options:['schwanger', 'sich langweilen','nah','geschieden' ],
+        q:'Comment dit-on l’interrogatoire',
+        options:['das Verhör', 'minderjährig','die Hauptaufgabe','die Auslandsreise' ],
         answer:0
     }, 
     {
-        q:'Traduire wegen',
-        options:['à cause de', 'beaucoup à faire','préparer','passer' ],
+        q:'Comment dit-on la pression',
+        options:['der Druck', 'erpressen','die Schauspielerin','schweigen' ],
         answer:0
     }, 
+
+
 	{
-		q:'Comment dit-on comprendre ?',
-		options:['deshalb', 'verbringen','fragen',"verstehen" ],
-		answer:3
+		q:'Comment dit-on faire du chantage ?',
+		options:['erpressen', 'verbringen','fragen',"verstehen" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on déménager ?',
-		options:['ganzen', 'verbringen','zehen',"ziehen" ],
-		answer:3
+		q:'Comment dit-on le poête ?',
+		options:['der Dichter ', ' die Badewanne','Der Dusche',"der Druck" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on dans le monde ?',
-		options:['ganzen', 'der Gründer','der Wald',"das Welt" ],
-		answer:3
+		q:'Comment dit-on  la prison ?',
+		options:['das Gefängnis ', ' das Arbeitslosigkeit ','der Beruf',"Der Gründer" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  l\'experience ?',
-		options:['die Zahl', 'die Gemeinschaft','die Wohngemeinschaft',"die Erfahrung" ],
-		answer:3
+		q:'Que veut dire jdn verhaften ?',
+		options:['arrêter qqn ', 'faire taire qqn','surveiller qqn',"enfermer qqn" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  le nombre ?',
-		options:['die Erfahrung', 'unter','die Beerdigung',"die Zahl" ],
-		answer:3
+		q:'Comment dit-on courageux ?',
+		options:['mutig', ' schweigen','gefährlich',"beweisen" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  l\'enterrement ?',
-		options:['die Erfahrung', 'Weihnachten','die Miete',"die Beerdigung" ],
-		answer:3
+		q:'Que veut dire ängstlich ?',
+		options:['anxieux', 'courageux','peureux',"puissant" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  le loyer ?',
-		options:['die Zeitschrift', 'die Zahl','die Erde',"die Miete" ],
-		answer:3
+		q:'Comment dit-on l’influence ?',
+		options:['der Einfluss ', 'der Handschuh','der Handschuh',"beeinflüssen" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  la terre ?',
-		options:['der Beruf', 'der Grund','die Miete',"die Erde" ],
-		answer:3
+		q:'Que veut dire besorgt ?',
+		options:['inquiet', 'solitaire','mineur',"prouver" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on  le métier ?',
-		options:['ziehen', 'die Beerdigung','der Gründer',"die Beruf" ],
-		answer:3
+		q:'Comment dit-on le couteau ?',
+		options:['das Messer ', 'das Blut ','die Traumwelt ',"ein Geisterpferd" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on pas le temps ?',
-		options:['nicht Zeit haben', 'nicht Zeit','ohne Zeit',"keine Zeit" ],
-		answer:3
+		q:'Comment dit-on mineur ?',
+		options:['minderjährig', 'beweisen','mutig',"hochzuklettern" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on pas le temps ?',
-		options:['nicht Zeit haben', 'nicht Zeit','ohne Zeit',"keine Zeit" ],
-		answer:3
+		q:'Comment dit-on disparu ?',
+		options:['verschwunden', 'verurteilen','Verhalten',"verurteilen" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on a cause de ?',
-		options:['allein', 'jünger','bewegen',"wegen" ],
-		answer:3
+		q:'Comment dit-on le visage ?',
+		options:['das Gesicht','Die Gründer','das Bein',"der Ellenbogen " ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on seul ?',
-		options:['bewegen', 'jünger','von',"allein" ],
-		answer:3
+		q:'Comment dit-on prouver ?',
+		options:['beweisen','verschwunden','verurteilen',"Verhalten" ],
+		answer:0
 	},
 	{
-		q:'Comment dit-on noël ?',
-		options:['tannenbaum', 'den Tan','die Gemeinschaft',"Weihnachten" ],
-		answer:3
-	}
+		q:'Comment dit-on elle se réveille ?',
+		options:['sie wacht auf','sie sicht auf','sie hinlegen auf',"sie hinsetzen auf" ],
+		answer:0
+	},
+	{
+		q:'Que veut dire das Gemälde ?',
+		options:['le tableau','le cauchemar','l\'invisible',"la réalité" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on le cauchemar ?',
+		options:['der Alptraum','das Gemälde','Außerdem',"beeinflüssen" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on représenter ?',
+		options:['dar-stellen','der Vergleich','im Hintergrund',"beeinflüssen" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on le de plus, en outre ?',
+		options:['Außerdem','beeinflüssen','minderjährig',"deshalb" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on montrer ?',
+		options:['zeigen','hinlegen','hinsetzen',"hüpfen" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on l‘invisible ?',
+		options:['das Unsichtbare','Geisterpferd','gewußt',"das Gemälde" ],
+		answer:0
+	},
+	{
+		q:'Comment dit-on les la réalité ?',
+		options:['die Wirklichkeit','die Traumwelt','Der Urlaub',"der Gold" ],
+		answer:0
+	},
+
+
+
 ]
